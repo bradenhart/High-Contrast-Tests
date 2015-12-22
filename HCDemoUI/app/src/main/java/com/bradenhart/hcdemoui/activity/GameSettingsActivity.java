b74a3155e313a58747b86e740501214386efda94
BRADEN HART <bradendhart@gmail.com>
2015-12-23 00:23:24 +1300

    tested icon card view with selectors in Settings activity as there is nothing else there yet

--- HCDemoUI/app/src/main/java/com/bradenhart/hcdemoui/activity/GameSettingsActivity.java
+++ HCDemoUI/app/src/main/java/com/bradenhart/hcdemoui/activity/GameSettingsActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.bradenhart.hcdemoui.R;
@@ -15,9 +18,10 @@ import com.bradenhart.hcdemoui.adapter.viewholder.RecyclerItemViewHolder;
  */
 public class GameSettingsActivity extends BaseActivity implements View.OnClickListener {
 
-    private LinearLayout splitCardView;
-    private TextView filterHeader, sortHeader;
-    private LinearLayout filterLayout, sortLayout;
+    private RelativeLayout filterCard;
+    private ImageView filterTab, sortTab;
+    private ScrollView filterLayout, sortLayout;
+    private TextView cardHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +30,19 @@ public class GameSettingsActivity extends BaseActivity implements View.OnClickLi
 
         updateCheckedDrawerItem(R.id.nav_settings);
 
-        splitCardView = (LinearLayout) findViewById(R.id.split_card_view);
-        filterHeader = (TextView) findViewById(R.id.filter_by_header);
-        sortHeader = (TextView) findViewById(R.id.sort_by_header);
-        filterLayout = (LinearLayout) findViewById(R.id.filter_button_layout);
-        sortLayout = (LinearLayout) findViewById(R.id.sort_button_layout);
+        filterCard = (RelativeLayout) findViewById(R.id.icon_card_view);
+        filterTab = (ImageView) findViewById(R.id.filter_icon_tab);
+        sortTab = (ImageView) findViewById(R.id.sort_icon_tab);
+        filterLayout = (ScrollView) findViewById(R.id.filter_layout);
+        sortLayout = (ScrollView) findViewById(R.id.sort_layout);
+        cardHeader = (TextView) findViewById(R.id.icon_card_header);
+
+        filterTab.setSelected(true);
+
+        filterTab.setOnClickListener(this);
+        sortTab.setOnClickListener(this);
 
         findViewById(R.id.base_fab).setOnClickListener(this);
-        filterHeader.setOnClickListener(this);
-        sortHeader.setOnClickListener(this);
-
 
     }
 
@@ -51,53 +58,68 @@ public class GameSettingsActivity extends BaseActivity implements View.OnClickLi
 
         switch (id) {
             case R.id.base_fab:
-                if (splitCardView.getVisibility() == View.VISIBLE) {
-                    showSplitCardView();
+                if (filterCard.getVisibility() == View.VISIBLE) {
+                    closeFilterCard();
                 } else {
-                    hideSplitCardView();
+                    openFilterCard();
                 }
                 break;
-            case R.id.filter_by_header:
-                handleFilterHeaderClick();
+            case R.id.filter_icon_tab:
+                if (filterTab.isSelected()) {
+//                    filterTab.setSelected(false);
+//                    sortTab.setSelected(true);
+                    hideFilterLayout();
+                    showSortLayout();
+                } else {
+                    hideSortLayout();
+                    showFilterLayout();
+                }
                 break;
-            case R.id.sort_by_header:
-                handleSortHeaderClick();
+            case R.id.sort_icon_tab:
+                if (sortTab.isSelected()) {
+//                    sortTab.setSelected(false);
+//                    filterTab.setSelected(true);
+                    hideSortLayout();
+                    showFilterLayout();
+                } else {
+                    hideFilterLayout();
+                    showSortLayout();
+                }
                 break;
         }
     }
 
-    private void showSplitCardView() {
+    private void openFilterCard() {
+        filterCard.setVisibility(View.VISIBLE);
+        filterTab.setSelected(true);
         filterLayout.setVisibility(View.VISIBLE);
         sortLayout.setVisibility(View.GONE);
-        splitCardView.setVisibility(View.GONE);
     }
 
-    private void hideSplitCardView() {
+    private void closeFilterCard() {
+        filterCard.setVisibility(View.GONE);
+    }
+
+    private void showFilterLayout() {
+        sortTab.setSelected(false);
+        filterTab.setSelected(true);
         filterLayout.setVisibility(View.VISIBLE);
+        cardHeader.setText("Filter");
+    }
+
+    private void showSortLayout() {
+        filterTab.setSelected(false);
+        sortTab.setSelected(true);
+        sortLayout.setVisibility(View.VISIBLE);
+        cardHeader.setText("Sort");
+    }
+
+    private void hideFilterLayout() {
+        filterLayout.setVisibility(View.GONE);
+    }
+
+    private void hideSortLayout() {
         sortLayout.setVisibility(View.GONE);
-        splitCardView.setVisibility(View.VISIBLE);
-    }
-
-    private void handleFilterHeaderClick() {
-        if (filterLayout.getVisibility() == View.VISIBLE) {
-            filterLayout.setVisibility(View.GONE);
-        } else {
-            filterLayout.setVisibility(View.VISIBLE);
-        }
-        if (sortLayout.getVisibility() == View.VISIBLE) {
-            sortLayout.setVisibility(View.GONE);
-        }
-    }
-
-    private void handleSortHeaderClick() {
-        if (sortLayout.getVisibility() == View.VISIBLE) {
-            sortLayout.setVisibility(View.GONE);
-        } else {
-            sortLayout.setVisibility(View.VISIBLE);
-        }
-        if (filterLayout.getVisibility() == View.VISIBLE) {
-            filterLayout.setVisibility(View.GONE);
-        }
     }
 
 }
